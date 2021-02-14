7cfbe46c7f0bac4dfcf781bc1ca3ab19455aaa6f
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-02-13 20:28:23 -0600

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Counter } from './components/Counter';
 import { Timer } from './components/Timer';
 import { User } from './components/User';
+import { CounterReducer } from './components/CounterReducer';
 
 function App() {
   return (
@@ -10,10 +11,15 @@ function App() {
       <hr/>
       <h2>useState!!</h2>
       <Counter/>
+      <hr/>
       <User/>
       <hr/>
       <h2>useEffect!!</h2>
       <Timer/>
+      <hr/>
+      <h2>useReducer!!</h2>
+      <CounterReducer/>
+      <hr/>
     </>
   );
 }
---
+++ src/components/CounterReducer.tsx
@@ -0,0 +1,70 @@
+import React,{ useReducer } from 'react';
+import { Counter } from './Counter';
+
+const initialState = {
+    contador: 10,
+}
+// | <= means OR
+type ActionType = 
+    | { type: 'incrementar' }
+    | { type: 'decrementar' }
+    | { type: 'custom', payload: number };
+
+
+
+const contadorReducer = ( state: typeof initialState, action: ActionType ) => {
+
+    switch ( action.type ) {
+        case 'incrementar':
+            return {
+                ...state,
+                contador: state.contador + 1
+            }
+
+
+        case 'decrementar':
+            return {
+                ...state,
+                contador: state.contador - 1
+            }
+
+        case 'custom':
+            return {
+                ...state,
+                contador: action.payload
+            }
+            
+    
+        default:
+            return state;
+    }
+}
+
+
+
+
+
+export const CounterReducer = () => {
+
+    const [ { contador }, dispatch] = useReducer(contadorReducer, initialState);
+
+
+    return (
+        <>
+         <h2>Contador: { contador }</h2> 
+         <button className="btn btn-outline-primary"
+                onClick={ () => dispatch({ type: 'incrementar' }) }>
+            +1
+         </button>
+         <button className="btn btn-outline-primary"
+                onClick={ () => dispatch({ type: 'decrementar' }) }>
+            -1
+         </button>
+
+         <button className="btn btn-outline-danger"
+                onClick={ () => dispatch({ type: 'custom', payload: 100 }) }>
+            100
+         </button>
+        </>
+    )
+}
